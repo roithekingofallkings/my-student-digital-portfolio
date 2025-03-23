5511694b66ef52ba7d186a646d4e74602f0a5f59
roithekingofallkings <joshuajamero81@gmail.com>
2025-03-23 16:55:55 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function sayYes() {
+    alert("Looking Forward to hear from You!")    
+    window.open('https://www.facebook.com/roijoshuajamero');
+}
+
+function sayNo() {
+    alert("Looking Forward to hear from You!")
+    window.open('https://www.instagram.com/joshuaroi01/')
+}
